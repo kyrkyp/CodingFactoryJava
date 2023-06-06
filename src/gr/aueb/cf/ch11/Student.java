33778eccede9444d96244f83dd94e08a32b8fe23
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-06-06 19:46:46 +0300

    Second commit

---
+++ src/gr/aueb/cf/ch11/Student.java
@@ -0,0 +1,61 @@
+package gr.aueb.cf.ch11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Definition of a simple student class
+ * POJO (Plain Old Java Object)
+ * Student Java Bean
+ */
+public class Student {
+    private final static int studentsCount;
+    static Scanner in = new Scanner(System.in);
+
+    static {
+        int num = in.nextInt();
+        studentsCount = (num == 1) ? 1 : 0;
+    }
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Student(){
+//        studentsCount++;
+    }
+
+    public Student(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+//        studentsCount++;
+    }
+
+    public static int getStudentsCount() {
+        return studentsCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
